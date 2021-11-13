5be946be6f11894b6bcfbda9aa60d5e950e7871c
Kürşat Demirer <krstdmrr@gmail.com>
2021-11-13 16:32:14 +0300

    Initial MVC

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>First Laravel Page</title>
+</head>
+<body>
+
+<h1>Welcome to Laravel</h1>
+<p>This is a paragraph.</p>
+<a href="{{route('test',['id'=>27,'name'=>'Kursat'])}}">Test Sayfa</a>
+</body>
+</html>
